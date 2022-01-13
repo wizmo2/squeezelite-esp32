3fb1c16f56d683028b553b064ce0e5f206d53be3
Philippe G <philippe44@users.noreply.github.com>
2022-01-13 13:08:45 -0800

    get ready for 4.4

--- components/platform_console/platform_console.c
+++ components/platform_console/platform_console.c
@@ -290,10 +290,10 @@ void initialize_console() {
 	/* Configure UART. Note that REF_TICK is used so that the baud rate remains
 	 * correct while APB frequency is changing in light sleep mode.
 	 */
-	const uart_config_t uart_config = { .baud_rate =
-			CONFIG_ESP_CONSOLE_UART_BAUDRATE, .data_bits = UART_DATA_8_BITS,
+	const uart_config_t uart_config = { .baud_rate = CONFIG_ESP_CONSOLE_UART_BAUDRATE, 
+			.data_bits = UART_DATA_8_BITS,
 			.parity = UART_PARITY_DISABLE, .stop_bits = UART_STOP_BITS_1,
-			.use_ref_tick = true };
+	};
 	ESP_ERROR_CHECK(uart_param_config(CONFIG_ESP_CONSOLE_UART_NUM, &uart_config));
 
 	/* Install UART driver for interrupt-driven reads and writes */
