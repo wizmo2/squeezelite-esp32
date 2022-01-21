47774c98f0633dc8376aa2588eb5263a5c5b6152
Philippe G <philippe44@users.noreply.github.com>
2022-01-20 17:34:21 -0800

    make targets "loadable"

--- components/targets/init.c
+++
@@ -1,3 +0,0 @@
-// weak should do the job but it does not...
- __attribute__((weak)) void target_init(void) { 
-}
---
+++ components/targets/targets.c
@@ -0,0 +1,11 @@
+#include "string.h"
+#include "targets.h"
+
+const struct target_s *target_set[] = { &target_muse, NULL };
+
+void target_init(char *target) { 
+	for (int i = 0; target_set[i]; i++) if (strcasestr(target_set[i]->model, target)) {
+		target_set[i]->init();
+		break;
+	}	
+}
