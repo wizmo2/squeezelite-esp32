28dba930c789500ff8b0474c4ffa700532ad126a
Sebastien L <sle118@hotmail.com>
2022-01-27 21:03:32 -0500

    .3: Auto stash before merge of "master-v4.3" and "origin/master-v4.3"

--- components/wifi-manager/webapp/webapp.cmake
+++ components/wifi-manager/webapp/webapp.cmake
@@ -1,4 +1,4 @@
 target_add_binary_data( __idf_wifi-manager webapp/dist/css/index.eb4a1bb223a59c656f2c.css.gz BINARY)
 target_add_binary_data( __idf_wifi-manager webapp/dist/favicon-32x32.png BINARY)
 target_add_binary_data( __idf_wifi-manager webapp/dist/index.html.gz BINARY)
-target_add_binary_data( __idf_wifi-manager webapp/dist/js/index.6409a9.bundle.js.gz BINARY)
+target_add_binary_data( __idf_wifi-manager webapp/dist/js/index.0ff7b6.bundle.js.gz BINARY)
--- components/wifi-manager/webapp/webpack.c
+++ components/wifi-manager/webapp/webpack.c
@@ -6,24 +6,24 @@ extern const uint8_t _favicon_32x32_png_start[] asm("_binary_favicon_32x32_png_s
 extern const uint8_t _favicon_32x32_png_end[] asm("_binary_favicon_32x32_png_end");
 extern const uint8_t _index_html_gz_start[] asm("_binary_index_html_gz_start");
 extern const uint8_t _index_html_gz_end[] asm("_binary_index_html_gz_end");
-extern const uint8_t _index_6409a9_bundle_js_gz_start[] asm("_binary_index_6409a9_bundle_js_gz_start");
-extern const uint8_t _index_6409a9_bundle_js_gz_end[] asm("_binary_index_6409a9_bundle_js_gz_end");
+extern const uint8_t _index_0ff7b6_bundle_js_gz_start[] asm("_binary_index_0ff7b6_bundle_js_gz_start");
+extern const uint8_t _index_0ff7b6_bundle_js_gz_end[] asm("_binary_index_0ff7b6_bundle_js_gz_end");
 const char * resource_lookups[] = {
 	"/css/index.eb4a1bb223a59c656f2c.css.gz",
 	"/favicon-32x32.png",
 	"/index.html.gz",
-	"/js/index.6409a9.bundle.js.gz",
+	"/js/index.0ff7b6.bundle.js.gz",
 ""
 };
 const uint8_t * resource_map_start[] = {
 	_index_eb4a1bb223a59c656f2c_css_gz_start,
 	_favicon_32x32_png_start,
 	_index_html_gz_start,
-	_index_6409a9_bundle_js_gz_start
+	_index_0ff7b6_bundle_js_gz_start
 };
 const uint8_t * resource_map_end[] = {
 	_index_eb4a1bb223a59c656f2c_css_gz_end,
 	_favicon_32x32_png_end,
 	_index_html_gz_end,
-	_index_6409a9_bundle_js_gz_end
+	_index_0ff7b6_bundle_js_gz_end
 };
