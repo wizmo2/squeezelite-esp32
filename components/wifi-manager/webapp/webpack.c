168b2fa50c4d3a57397eaf2dae7534e87cd219f4
Wizmo2 <wizmo.home@yahoo.com>
2023-10-14 17:37:55 -0400

    webapp rebuild

--- components/wifi-manager/webapp/webpack.c
+++ components/wifi-manager/webapp/webpack.c
@@ -1,34 +1,34 @@
 // Automatically generated. Do not edit manually!.
 #include <inttypes.h>
-extern const uint8_t _index_9de57ccd5f94250a38d0_css_gz_start[] asm("_binary_index_9de57ccd5f94250a38d0_css_gz_start");
-extern const uint8_t _index_9de57ccd5f94250a38d0_css_gz_end[] asm("_binary_index_9de57ccd5f94250a38d0_css_gz_end");
+extern const uint8_t _index_79b066409aae272d620d_css_gz_start[] asm("_binary_index_79b066409aae272d620d_css_gz_start");
+extern const uint8_t _index_79b066409aae272d620d_css_gz_end[] asm("_binary_index_79b066409aae272d620d_css_gz_end");
 extern const uint8_t _favicon_32x32_png_start[] asm("_binary_favicon_32x32_png_start");
 extern const uint8_t _favicon_32x32_png_end[] asm("_binary_favicon_32x32_png_end");
 extern const uint8_t _index_html_gz_start[] asm("_binary_index_html_gz_start");
 extern const uint8_t _index_html_gz_end[] asm("_binary_index_html_gz_end");
-extern const uint8_t _index_1d06b9_bundle_js_gz_start[] asm("_binary_index_1d06b9_bundle_js_gz_start");
-extern const uint8_t _index_1d06b9_bundle_js_gz_end[] asm("_binary_index_1d06b9_bundle_js_gz_end");
-extern const uint8_t _node_vendors_1d06b9_bundle_js_gz_start[] asm("_binary_node_vendors_1d06b9_bundle_js_gz_start");
-extern const uint8_t _node_vendors_1d06b9_bundle_js_gz_end[] asm("_binary_node_vendors_1d06b9_bundle_js_gz_end");
+extern const uint8_t _index_27f381_bundle_js_gz_start[] asm("_binary_index_27f381_bundle_js_gz_start");
+extern const uint8_t _index_27f381_bundle_js_gz_end[] asm("_binary_index_27f381_bundle_js_gz_end");
+extern const uint8_t _node_vendors_27f381_bundle_js_gz_start[] asm("_binary_node_vendors_27f381_bundle_js_gz_start");
+extern const uint8_t _node_vendors_27f381_bundle_js_gz_end[] asm("_binary_node_vendors_27f381_bundle_js_gz_end");
 const char * resource_lookups[] = {
-	"/css/index.9de57ccd5f94250a38d0.css.gz",
+	"/css/index.79b066409aae272d620d.css.gz",
 	"/favicon-32x32.png",
 	"/index.html.gz",
-	"/js/index.1d06b9.bundle.js.gz",
-	"/js/node_vendors.1d06b9.bundle.js.gz",
+	"/js/index.27f381.bundle.js.gz",
+	"/js/node_vendors.27f381.bundle.js.gz",
 ""
 };
 const uint8_t * resource_map_start[] = {
-	_index_9de57ccd5f94250a38d0_css_gz_start,
+	_index_79b066409aae272d620d_css_gz_start,
 	_favicon_32x32_png_start,
 	_index_html_gz_start,
-	_index_1d06b9_bundle_js_gz_start,
-	_node_vendors_1d06b9_bundle_js_gz_start
+	_index_27f381_bundle_js_gz_start,
+	_node_vendors_27f381_bundle_js_gz_start
 };
 const uint8_t * resource_map_end[] = {
-	_index_9de57ccd5f94250a38d0_css_gz_end,
+	_index_79b066409aae272d620d_css_gz_end,
 	_favicon_32x32_png_end,
 	_index_html_gz_end,
-	_index_1d06b9_bundle_js_gz_end,
-	_node_vendors_1d06b9_bundle_js_gz_end
+	_index_27f381_bundle_js_gz_end,
+	_node_vendors_27f381_bundle_js_gz_end
 };
--- components/wifi-manager/webapp/webpack.h
+++ components/wifi-manager/webapp/webpack.h
@@ -1,6 +1,6 @@
 /***********************************
 webpack_headers
-dist/css/index.9de57ccd5f94250a38d0.css.gz,dist/favicon-32x32.png,dist/index.html.gz,dist/js/index.1d06b9.bundle.js.gz,dist/js/node_vendors.1d06b9.bundle.js.gz
+dist/css/index.79b066409aae272d620d.css.gz,dist/favicon-32x32.png,dist/index.html.gz,dist/js/index.27f381.bundle.js.gz,dist/js/node_vendors.27f381.bundle.js.gz
 ***********************************/
 #pragma once
 #include <inttypes.h>
